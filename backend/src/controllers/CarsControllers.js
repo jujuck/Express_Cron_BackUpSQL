2afa202b1978a53b5814f738bbc054ae8375ef9b
Julien RICHARD <julien.y.richard@gmail.com>
2023-02-24 15:26:05 +0100

    generating the tempalte for the workshop

---
+++ backend/src/controllers/CarsControllers.js
@@ -0,0 +1,111 @@
+const models = require("../models");
+const logger = require("../services/logger");
+const validate = require("../services/cars");
+
+const browse = (req, res) => {
+  models.cars
+    .findAll(req.query.page)
+    .then(([cars]) => {
+      models.cars
+        .findCount()
+        .then(([count]) => {
+          res.status(200).json({ cars, pages: Math.ceil(count[0].pages / 50) });
+        })
+        .catch((err) => {
+          logger.error(err);
+          res.sendStatus(500);
+        });
+    })
+    .catch((err) => {
+      logger.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const read = (req, res) => {
+  models.cars
+    .find(req.params.id)
+    .then(([rows]) => {
+      if (rows[0] == null) {
+        res.sendStatus(404);
+      } else {
+        res.send(rows[0]);
+      }
+    })
+    .catch((err) => {
+      logger.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const add = (req, res) => {
+  const car = req.body;
+
+  const error = validate(car, "required");
+  if (!error) {
+    models.cars
+      .insert(car)
+      .then((createdCars) => {
+        res.status(200).send({
+          ...req.body,
+          id: createdCars[0].insertId,
+        });
+      })
+      .catch((err) => {
+        logger.error(err);
+        res.status(500).Send("Error on adding a new car");
+      });
+  } else {
+    logger.error(error.details);
+    res.status(422).send(error.details);
+  }
+};
+
+const deleteOne = (req, res) => {
+  const { id } = req.params;
+  models.cars
+    .delete(id)
+    .then((response) => {
+      if (response[0].affectedRows === 0) {
+        res.sendStatus(404);
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      logger.error(err);
+      res.status(500).send(err);
+    });
+};
+
+const update = (req, res) => {
+  const car = req.body;
+  const { id } = req.params;
+  const validation = validate(car, "optional");
+  if (validation) {
+    logger.error(validation.details);
+    res.status(422).send(validation);
+  } else {
+    models.cars
+      .update(car, id)
+      .then((updatedCar) => {
+        if (updatedCar[0].affectedRows === 0) {
+          res.sendStatus(404);
+        } else {
+          res.sendStatus(204);
+        }
+      })
+      .catch((err) => {
+        logger.error(err);
+        res.status(500).Send("Error on adding a new car");
+      });
+  }
+};
+
+module.exports = {
+  browse,
+  read,
+  add,
+  deleteOne,
+  update,
+};
